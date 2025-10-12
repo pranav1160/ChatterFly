10fd05aa06eb930ad745e3f0f6df439db0d6ba26
Pranav <pranav1160ly@gmail.com>
2025-10-12 19:49:25 +0530

    Initial Commit

---
+++ ChatterFly/ChatterFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterFlyApp.swift
+//  ChatterFly
+//
+//  Created by Pranav on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatterFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatterFlyTests/ChatterFlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterFlyTests.swift
+//  ChatterFlyTests
+//
+//  Created by Pranav on 12/10/25.
+//
+
+import Testing
+@testable import ChatterFly
+
+struct ChatterFlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
