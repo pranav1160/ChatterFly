45a71ef52c41f4575bbe045bc1ff4318eaa4ec2a
Pranav <pranav1160ly@gmail.com>
2025-12-23 19:42:46 +0530

    [Feature] recent avatars

--- ChatterFly/Core/ChatList/ChatListView.swift
+++ ChatterFly/Core/ChatList/ChatListView.swift
@@ -8,45 +8,98 @@
 import SwiftUI
 
 struct ChatListView: View {
-    @State private var chats = Chat.mocks
-    @State private var path : [NavigationPathOption] = []
+    
+    @State private var chats: [Chat] = Chat.mocks
+    @State private var recentAvatars: [Avatar] = Avatar.mocks
+    
+    @State private var path: [NavigationPathOption] = []
     
     var body: some View {
         NavigationStack(path: $path) {
             List {
-                ForEach(chats) { chat in
-                    
-                    ChatRowCellViewBuilder(
-                        currentUserId: nil,
-                        chat: chat,
-                        getAvatar: {
-                            // async fetch avatar
-                            try? await Task.sleep(for: .seconds(1))
-                            return Avatar.mocks.randomElement()!
-                        },
-                        getLastMessage: {
-                            // async fetch last message
-                            try? await Task.sleep(for: .seconds(1))
-                            return ChatMessage.mocks.randomElement()!
-                        }
-                    )
-                    .anyButton(style: .highlight) {
-                        onChatPressed(chat: chat)
-                    }
+                if !recentAvatars.isEmpty {
+                    recentsSection
                 }
-                .buttonStyle(.plain)
+                chatsSection
             }
-            
-            .listStyle(.plain)
             .navigationTitle("Chats")
             .navigationDestinationForCoreModule(path: $path)
         }
     }
     
-    private func onChatPressed(chat:Chat){
+    private var chatsSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("Your chats will appear here!")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+                    .removeExtraListFormatting()
+            } else {
+                ForEach(chats) { chat in
+                    ChatRowCellViewBuilder(
+                        currentUserId: nil, // Add cuid
+                        chat: chat,
+                        getAvatar: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return Avatar.mocks.randomElement()!
+                        },
+                        getLastMessage: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return ChatMessage.mocks.randomElement()!
+                        }
+                    )
+                    .anyButton(style: .highlight, action: {
+                        onChatPressed(chat: chat)
+                    })
+                    .removeExtraListFormatting()
+                }
+            }
+        } header: {
+            Text("Chats")
+        }
+    }
+    
+    private var recentsSection: some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let imageName = avatar.profileImageName {
+                            VStack(spacing: 8) {
+                                ImageLoaderView(url: imageName)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(Circle())
+                                
+                                Text(avatar.name ?? "")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .anyButton {
+                                onAvatarPressed(avatar: avatar)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            }
+            .frame(height: 120)
+            .scrollIndicators(.hidden)
+            .removeExtraListFormatting()
+        } header: {
+            Text("Recents")
+        }
+    }
+    
+    private func onChatPressed(chat: Chat) {
         path.append(.chat(avatarId: chat.avatarId))
     }
     
+    private func onAvatarPressed(avatar: Avatar) {
+        path.append(.chat(avatarId: avatar.avatarId))
+    }
 }
 
 
--- ChatterFly/Services/Avatar/Models/Avatar.swift
+++ ChatterFly/Services/Avatar/Models/Avatar.swift
@@ -13,7 +13,7 @@ struct Avatar:Hashable{
     let characterOption:CharacterOption?
     let characterAction:CharacterAction?
     let characterLocation:CharacterLocation?
-    let profileImageName:String?
+    var profileImageName:String?
     let authorId:String?
     let dateCreated:Date?
     
